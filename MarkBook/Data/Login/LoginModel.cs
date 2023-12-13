920240c9bfe2cf0ff1d36b02eec7a2103cd662c8
DaniilF2323@outlook.com <DaniilF2323@outlook.com>
2023-12-13 13:24:07 +0300

    Добавьте файлы проекта.

---
+++ MarkBook/Data/Login/LoginModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MarkBook.Data.Login
+{
+	public class LoginModel
+	{
+		[Required]
+		[MaxLength(20, ErrorMessage = "Логин слишком длинный!")]
+		public string Login { get; set; }
+
+		[Required]
+		[MaxLength(30, ErrorMessage = "Пароль слишком длинный!")]
+		public string Password { get; set; }
+	}
+}
